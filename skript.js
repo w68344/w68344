7ca46fb02986365c3d37fbe247144d9dccbbec3d
Nikita <127506511+w68344@users.noreply.github.com>
2023-07-10 14:51:08 +0200

    nr_10_07_2023

---
+++ skript.js
@@ -0,0 +1,27 @@
+// console.error("Pojawił błąd")
+// console.warn("Pamętaj o literówkach")
+// console.log("Cześć")
+// // Tu są komętarz
+// var a = 10
+// let b = 5
+// const pi = 3.141592
+
+
+// Calkulator na promtach
+// document.write("Odpowiedż: ")
+// let licz1 = parseFloat(prompt("Podaj liczbe 1"))
+// let licz2 = parseFloat(prompt("Podaj liczbe 2"))
+// let akt = prompt("Podaj znak" ,"+,-,/")
+// switch(akt)
+// {case "-": document.write(licz1-licz2);
+// break
+// case "+": document.write(licz1+licz2);
+// break
+// case "/": if (licz2==0) {document.write("Dzielenie przez zero jest niemożliwe!")} else {document.write(licz1/licz2)};
+// break
+// default: document.write("Operacjz jest niemożliwa")
+// }
+
+
+
+
