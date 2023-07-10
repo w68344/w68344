28223b768608c7a7f13d66923fad8ac148ddcd04
Nikita <127506511+w68344@users.noreply.github.com>
2023-07-10 16:57:39 +0200

    nr_10_07_2023

--- skript.js
+++ skript.js
@@ -1,3 +1,4 @@
+//Wprowadzienie do JS
 // console.error("Pojawił błąd")
 // console.warn("Pamętaj o literówkach")
 // console.log("Cześć")
@@ -21,6 +22,56 @@
 // break
 // default: document.write("Operacjz jest niemożliwa")
 // }
+
+//Działania na wyskakujących okienkach
+// alert("Jesteś programistem!")
+// if(confirm("Lubiś programować?")){document.write("Brawo TY")}
+// else {document.write("Zaczni to lubić!")}
+
+//dziawania na liczbach niecałkowitych
+// console.log((0.5).toFixed())
+// console.log((0.3).toFixed())
+// console.log((12.7).toFixed())
+// console.log((-8.8).toFixed())
+// console.log((0.5).toFixed())
+// console.log((0.3).toFixed(2))
+// console.log((12.7564).toFixed(2))
+// console.log((-8.8456).toFixed(3))
+
+//iterowanie tekstu
+// let text="Abagalamaga"
+
+// for(let i = 0; i < text.length; i++ )
+// {console.log(text[i])}
+
+// for( const t of text){console.log(t)}
+
+// for( let t of text){console.log(t)}
+
+
+//Operacje nad stringami
+// document.write("Enna ma kota <br>")
+// let text="Enna ma kota"
+// let tab = [...text]
+// tab[0] = "A"
+// let end_text = tab.join("")
+// document.write(end_text)
+// const age = 19;
+// const a = 'Ten chłopak ma ' + age +' lat';
+// const a = `Ten chłopak ma ${age} lat`
+// Очень вадно что бы кавычки были одинарными и наклонными!
+// console.log(a);
+// let text = "Jakiś text"
+// document.write(text.toUpperCase()+"<br>")
+// document.write(text.toLowerCase())
+
+
+
+
+
+
+
+
 
 
 
